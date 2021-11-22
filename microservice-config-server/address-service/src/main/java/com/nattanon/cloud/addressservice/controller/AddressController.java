5e76d41848eb5e37317156af15d755b4fa12226d
Nattanon Chansamakr <nattanon.ch@kbtg.tech>
2021-11-22 17:19:53 +0700

    Add special value for each profile & Add refresh scope for auto-update value using actuator

--- microservice-config-server/address-service/src/main/java/com/nattanon/cloud/addressservice/controller/AddressController.java
+++ microservice-config-server/address-service/src/main/java/com/nattanon/cloud/addressservice/controller/AddressController.java
@@ -4,11 +4,17 @@ import com.nattanon.cloud.addressservice.request.CreateAddressRequest;
 import com.nattanon.cloud.addressservice.response.AddressResponse;
 import com.nattanon.cloud.addressservice.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/address")
+@RefreshScope
 public class AddressController {
+
+    @Value("${spring.test}")
+    private String testValue;
 
     @Autowired
     private AddressService addressService;
@@ -21,5 +27,10 @@ public class AddressController {
     @GetMapping("/getById/{id}")
     public AddressResponse getById(@PathVariable long id) {
         return addressService.getById(id);
+    }
+
+    @GetMapping("/test")
+    public String getTestValue() {
+        return testValue;
     }
 }
